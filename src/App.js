644ecacf57f06b89cfae71bf9dd7f4d68c10838a
Ameer Ashif K <ameer.asif777@gmail.com>
2021-06-04 14:54:31 +0530

    Testing: Pushing build folder and manual setup in github page options under settings to deploy reactapp

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Let's Deploying react app in github page without using gh-pages
+    </div>
+  );
+}
+
+export default App;
